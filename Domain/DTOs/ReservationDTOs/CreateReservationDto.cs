f2afde7d985bb87d08a31c914ac24439301a8773
Mirzoalievb88 <bahodurmirzoaliev2008@gmail.com>
2025-06-30 15:43:57 +0500

    secomd cpmmit

--- Domain/DTOs/ReservationDTOs/CreateReservationDto.cs
+++ Domain/DTOs/ReservationDTOs/CreateReservationDto.cs
@@ -4,5 +4,7 @@ public class CreateReservationDto
 {
     public int TableId { get; set; }
     public int CustomerId { get; set; }
-    public DateTime ReservationDate { get; set; }
+    public DateTime ReservationDate { get; set; } = DateTime.UtcNow;
 }
+
+    
--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+088237e61bb201c1c28f6c5822eae5217ebf07d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5369c46dbf7b3d0404bacd30472c037054dc7da5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
